2547ccd0358f37c1cf3a25efecd1a0d5d6496f5b
gokulnaveen92 <115129138+gokulnaveen92@users.noreply.github.com>
2024-04-25 18:42:01 +0530

    Update Sort the Numbers in an ArrayList.java

--- Day 3/Session 3/Sort the Numbers in an ArrayList.java
+++ Day 3/Session 3/Sort the Numbers in an ArrayList.java
@@ -1,1 +1,46 @@
 
+// You are using Java
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        ArrayList<Integer> arrList = new ArrayList<>();
+        
+        for(int i=0;i<n;i++){
+            int ele = sc.nextInt();
+            arrList.add(ele);
+        }
+        
+        Collections.sort(arrList);
+        
+        int toSearch = sc.nextInt();
+        boolean isthere = arrList.contains(toSearch);
+        
+        if(isthere){
+            System.out.println("Found at position: "+(arrList.indexOf(toSearch)+1));
+            System.out.println(arrList);
+        }else{
+            for(int i=0;i<n;i++){
+                if(arrList.get(i)>toSearch){
+                    //System.out.println(arrList.get(i));
+                    arrList.add((i),toSearch);
+                    System.out.println("Not found, added at position: "+(i+1));
+                    break;
+                }else{
+                    if(arrList.get(n-1)<toSearch){
+                        arrList.add(toSearch);
+                        System.out.println("Not found, added at position: "+ (n+1));
+                        break;
+                        
+                    }
+                    
+                }
+            }
+            System.out.println(arrList);
+        }
+        
+        
+    }
+}
