f6177af38ee10068dffee3747674b1a48b1135cf
gokulnaveen92 <115129138+gokulnaveen92@users.noreply.github.com>
2024-04-28 12:04:15 +0530

    Update Email adrress.java

--- Day 4/session 3/Email adrress.java
+++ Day 4/session 3/Email adrress.java
@@ -1,5 +1,6 @@
 // You are using Java
 //validating Email Address custom Exception
+//USING STRING METHOD
  import java.util.*;
 import java.util.regex.*;
 
@@ -31,9 +32,9 @@ class Main{
             int count2=0;
             int count3=0;
             String email = sc.nextLine();
-            String sCrt = "^[a-zA-Z0-9]+@[a-zA-Z]+\\.(in|com|net|biz)$";
-            Pattern p1 = Pattern.compile(sCrt);
-            Matcher m1 = p1.matcher(email);
+            //String sCrt = "^[a-zA-Z0-9]+@[a-zA-Z]+\\.(in|com|net|biz)$";
+            //Pattern p1 = Pattern.compile(sCrt);
+            //Matcher m1 = p1.matcher(email);
             String[] dom = {"in","com","net","biz"};
 
            
@@ -65,6 +66,60 @@ class Main{
                 throw new DomainException("Invalid Domain \nInvalid email address");
             }else{
                 System.out.println("Valid email address");
+            }
+        } catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
+//USING REGEX 
+import java.util.*;
+import java.util.regex.*;
+ 
+class DotException extends Exception{
+    public DotException(String msg){
+        super(msg);
+    }
+}
+ 
+class AtTheRateException extends Exception{
+    public AtTheRateException(String msg){
+        super(msg);
+    }
+}
+ 
+class DomainException extends Exception{
+    public DomainException(String msg){
+        super(msg);
+    }
+}
+ 
+class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        try{
+            String email = sc.nextLine();
+            String sCrt = "^[a-zA-Z0-9]+@[a-zA-Z]+\\.(in|com|net|biz)$";
+            //Pattern p1 = Pattern.compile(sCrt);
+            //Matcher m1 = p1.matcher(email);
+            int count1=0;
+            int count2=0;
+            for(int i=0;i<email.length();i++){
+                if(email.charAt(i)=='@'){
+                    count1++;
+                }
+                if(email.charAt(i)=='.'){
+                    count2++;
+                }
+            }
+            if(Pattern.matches(sCrt,email)){
+                System.out.println("Valid email address");
+            }else if(count2!=1){
+                throw new DotException("Invalid Dot usage \nInvalid email address");
+            }else if(count1!=1){
+                throw new AtTheRateException("Invalid @ usage \nInvalid email address");
+            }else{
+                throw new DomainException("Invalid Domain \nInvalid email address");
             }
         } catch(Exception e){
             System.out.println(e);
